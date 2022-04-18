dc3c215a44a715db137a787001f082cc24738ef6
Alamgir Ahamed Saif <88821633+binarybunon@users.noreply.github.com>
2022-04-18 21:09:40 +0600

    custer

--- index.js
+++ index.js
@@ -1,40 +1,34 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const mongoose = require("mongoose");
-const Pusher = require("pusher");
-const cors = require("cors");
-const socketio = require("socket.io");
-const { v4 } = require("uuid");
+const mongoose = require('mongoose');
+const Pusher = require('pusher');
+const cors = require('cors');
+const socketio = require('socket.io');
+const { v4 } = require('uuid');
 
 const port = process.env.PORT || 5000;
-const pusher = new Pusher({
-	appId: "1211907",
-	key: "42ff087f118be67c0315",
-	secret: "eb438b15df05ab708ab5",
-	cluster: "ap2",
-	useTLS: true,
-});
 
 // middleware
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 app.use(express.json());
 
 app.use(cors());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 const uri =
-	"mongodb+srv://admin:admin@cluster0.pnvh0.mongodb.net/sms-custer?retryWrites=true&w=majority";
+	'mongodb+srv://admin:admin@cluster0.pnvh0.mongodb.net/sms-custer?retryWrites=true&w=majority';
 
 // mongoodb connection
-mongoose
-	.connect(uri, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useFindAndModify: true,
-	})
-	.then((db) => {
-		console.log("Mongodb Connected");
-	});
+// mongoose
+// 	.connect(uri, {
+// 		useNewUrlParser: true,
+// 		useUnifiedTopology: true,
+// 		useFindAndModify: true,
+// 	})
+// 	.then((db) => {
+// 		console.log('Mongodb Connected');
+// 	});
+
 const messageSchema = mongoose.Schema(
 	{
 		number: {
@@ -54,39 +48,40 @@ const messageSchema = mongoose.Schema(
 		timestamps: true,
 	}
 );
-const Message = mongoose.model("sms", messageSchema);
+const Message = mongoose.model('sms', messageSchema);
 
 // home page
-app.get("/", (req, res) => {
-	res.render("home");
+app.get('/', (req, res) => {
+	res.render('home');
 });
 
-app.get("/send", (req, res) => {
-	res.render("socket");
+app.get('/send', (req, res) => {
+	res.render('socket');
 });
-app.get("/bulk-send", (req, res) => {
-	res.render("bulk");
+app.get('/bulk-send', (req, res) => {
+	res.render('bulk');
 });
 
-app.get("/report", (req, res) => {
-	res.render("report");
+app.get('/report', (req, res) => {
+	res.render('report');
 });
 
 // app.post("/send", async (req, res) => {
 // 	const { number, body } = req.body;
 // });
 
-app.get("/messages", async (req, res) => {
+app.get('/messages', async (req, res) => {
 	const messages = await Message.findOne({ status: false });
 	res.json(messages);
 });
 
-app.get("/message/all", async (req, res) => {
-	const messages = await Message.find();
-	res.json(messages);
+app.get('/message/all', async (req, res) => {
+	// const messages = await Message.find();
+	// res.json(messages);
+	res.send([]);
 });
 
-app.get("/mark-as-sent/:id", async (req, res) => {
+app.get('/mark-as-sent/:id', async (req, res) => {
 	try {
 		const message = await Message.findOne({ _id: req.params.id });
 		message.status = true;
@@ -99,49 +94,55 @@ app.get("/mark-as-sent/:id", async (req, res) => {
 
 //////////////////////////////===============================
 
-const server = app.listen(port, console.log("server running on port " + port));
+const server = app.listen(port, console.log('server running on port ' + port));
 
 const io = socketio(server);
 var messages;
 
-let interval = 15000;
-let totalTime = 0;
+// let interval = 15000;
+// let totalTime = 0;
 
-io.on("connection", (socket) => {
-	console.log("Connected", socket.id);
-	// socket.emit("all", messages);
-	socket.on("message", async (message) => {
-		const { number, body } = message;
-		const mst = await new Message({ number, body }).save();
-		messages = mst;
-		socket.broadcast.emit("all", messages);
-		socket.emit("sender", messages);
+let deviceList = [];
+
+io.on('connection', (socket) => {
+	console.log('Connected', socket.id);
+
+	socket.on('browser-client-connected', () => {
+		console.log('Browser client connected');
+		socket.emit('connected-device', deviceList);
 	});
 
-	socket.on("sync", () => {
-		console.log("Sync Request Received from ", socket.id);
-		socket.broadcast.emit("all", messages);
+	// Ping
+	socket.on('device-connect', (deviceInfo) => {
+		console.log('Device connected');
+		deviceList.push({ ...deviceInfo, status: 'online', socketId: socket.id });
+		socket.broadcast.emit('connected-device', deviceList);
 	});
 
-	socket.on("bulk", async (data) => {
-		totalTime = (data.length - 1) * interval;
-		console.log("total Time :", totalTime, "interval: ", interval);
-		await Message.insertMany(data);
-		let sending = setInterval(() => {
-			console.log("Running... with :", totalTime);
-			if (totalTime >= 0) {
-				// per interval emit request
-				console.log("Emiting...");
-				// lets Attemt to emit event
-				socket.broadcast.emit("all", { id: v4() });
-
-				totalTime = totalTime - interval;
-			} else {
-				console.log("Stop Emiting ");
-				clearInterval(sending);
+	socket.on('ping', (deviceInfo) => {
+		try {
+			const index = deviceList.findIndex(
+				(x) => String(x.id) === String(deviceInfo?.id)
+			);
+			console.log(index, deviceList[index]);
+			if (index > -1) {
+				deviceList[index].deviceName = deviceInfo.deviceName;
+				socket.broadcast.emit('connected-device', deviceList);
 			}
-		}, interval);
+		} catch (error) {}
 	});
-	// send Bulk
-	// Per 10 second send Request
+
+	socket.on('message', async (message) => {
+		const { number, body } = message;
+		// const mst = await new Message({ number, body }).save();
+		// messages = mst;
+		console.log('sending sms', message);
+		// socket.('send-sms', messages);
+		socket.broadcast.emit('send-sms', message);
+	});
+
+	// On Device Disconnect Remove Device
+	socket.on('disconnect', (info) => {
+		console.log('disconnect', info);
+	});
 });
